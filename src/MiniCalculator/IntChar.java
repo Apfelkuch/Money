dae9d22c783059e58971b076b41a128ed34170ec
Apfelkuch <spiele55337@freenet.de>
2021-09-02 12:24:32 +0200

    Add MiniCalculator, not finished. Not connected to the window.

---
+++ src/MiniCalculator/IntChar.java
@@ -0,0 +1,18 @@
+package MiniCalculator;
+
+public class IntChar {
+
+    public int integer;
+    public Character character;
+
+    public IntChar(int integer, char character) {
+        this.integer = integer;
+        this.character = character;
+    }
+
+    @Override
+    public String toString() {
+        return integer + ":" + character;
+    }
+
+}
